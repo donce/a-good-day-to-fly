3a2e4b91fd98ffd1da0ed131a6092e44b1f5ab66
Donatas <donce.lt@gmail.com>
2013-11-01 15:05:04 +0200

    flying

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,26 +2,39 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	float speed = 10;
 	
-	GameObject target;
-	double speed;
+	const float minSpeed = 10;
+	const float maxSpeed = 200;
+	const float rotationSpeed = 100;
+	const float acceleration = 100;
 	
-	// Use this for initialization
 	void Start () {
-		target = GameObject.FindGameObjectWithTag("Target");
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		//transform.LookAt(target.transform);
-		//transform.Rotate(new Vector3(0, 0, 1));
+		float rotation = rotationSpeed * Time.deltaTime;
 		if (Input.GetKey(KeyCode.A))
-			transform.Rotate(new Vector3(0, 0, -1));
+			transform.Rotate(new Vector3(0, -rotation, 0));
 		if (Input.GetKey(KeyCode.D))
-			transform.Rotate(new Vector3(0, 0, 1));
+			transform.Rotate(new Vector3(0, rotation, 0));
+		if (Input.GetKey(KeyCode.J))
+			transform.Rotate(new Vector3(0, 0, rotation));
+		if (Input.GetKey(KeyCode.L))
+			transform.Rotate(new Vector3(0, 0, -rotation));
 		if (Input.GetKey(KeyCode.W))
-			transform.Rotate(new Vector3(1, 0, 0));
+			transform.Rotate(new Vector3(-rotation, 0, 0));
 		if (Input.GetKey(KeyCode.S))
-			transform.Rotate(new Vector3(-1, 0, 0));
+			transform.Rotate(new Vector3(rotation, 0, 0));
+		
+		
+		if (Input.GetKey(KeyCode.I))
+			speed += acceleration * Time.deltaTime;
+		if (Input.GetKey(KeyCode.K))
+			speed -= acceleration * Time.deltaTime;
+		speed = Mathf.Max(minSpeed, speed);
+		speed = Mathf.Min(maxSpeed, speed);
+		
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
 	}
 }
