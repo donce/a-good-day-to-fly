cae30a9a0733dfa6095d89efb113e25902ec650f
Donatas <donce.lt@gmail.com>
2013-11-01 13:22:27 +0200

    player rotation, collectable prefab

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+	
+	int time = 0;
+	Vector3 position;
+	
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, 1, 0));
+		transform.position.Set(position.x, position.y + Mathf.Sin(time), position.z);
+		time += 1;
+		//transform.localPosition.Set(pos.
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	GameObject target;
+	double speed;
+	
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Target");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.LookAt(target.transform);
+		//transform.Rotate(new Vector3(0, 0, 1));
+		if (Input.GetKey(KeyCode.A))
+			transform.Rotate(new Vector3(0, 0, -1));
+		if (Input.GetKey(KeyCode.D))
+			transform.Rotate(new Vector3(0, 0, 1));
+		if (Input.GetKey(KeyCode.W))
+			transform.Rotate(new Vector3(1, 0, 0));
+		if (Input.GetKey(KeyCode.S))
+			transform.Rotate(new Vector3(-1, 0, 0));
+	}
+}
