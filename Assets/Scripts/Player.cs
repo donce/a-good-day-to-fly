50b9eafcfbe94bf8dd58b34bd812dce8503ec46c
Donatas <donce.lt@gmail.com>
2013-11-01 17:47:44 +0200

    Particles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	float speed = 10;
+	private float speed = 10;
 	
 	const float minSpeed = 10;
 	const float maxSpeed = 200;
 	const float rotationSpeed = 100;
+	
 	const float acceleration = 100;
+	const float slowingSpeed = 20;
 	
 	void Start () {
 	}
@@ -23,15 +25,16 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.L))
 			rigidbody.transform.Rotate(Vector3.forward, -rotation);
 		if (Input.GetKey(KeyCode.W))
-			rigidbody.transform.Rotate(Vector3.right, -rotation);
-		if (Input.GetKey(KeyCode.S))
 			rigidbody.transform.Rotate(Vector3.right, rotation);
+		if (Input.GetKey(KeyCode.S))
+			rigidbody.transform.Rotate(Vector3.right, -rotation);
 		
-		
+		float speedChange = slowingSpeed;
 		if (Input.GetKey(KeyCode.I))
-			speed += acceleration * Time.deltaTime;
+			speedChange = acceleration;
 		if (Input.GetKey(KeyCode.K))
-			speed -= acceleration * Time.deltaTime;
+			speedChange = -acceleration;
+		speed += speedChange * Time.deltaTime;
 		speed = Mathf.Max(minSpeed, speed);
 		speed = Mathf.Min(maxSpeed, speed);
 		
