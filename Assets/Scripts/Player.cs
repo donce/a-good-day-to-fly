2e1cc468012fab4aeb3ac803251f45c735ae1be5
Donatas <donce.lt@gmail.com>
2013-11-02 14:41:25 +0200

    SciFi Fighter asset, removed assets, world generator class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,7 +29,7 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.S))
 			rigidbody.transform.Rotate(Vector3.right, -rotation);
 		
-		float speedChange = slowingSpeed;
+		float speedChange = -slowingSpeed;
 		if (Input.GetKey(KeyCode.I))
 			speedChange = acceleration;
 		if (Input.GetKey(KeyCode.K))
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour {
+	
+	void Awake() {
+		Generate();
+	}
+	
+	void Generate() {
+		Debug.Log("Generating");
+	}
+	
+}
