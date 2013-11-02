327007b5b4f29a8918b32aaeb27597477c436460
Donatas <donce.lt@gmail.com>
2013-11-02 15:13:36 +0200

    WorldGenerator base

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -3,10 +3,7 @@ using System.Collections;
 
 public class Collectable : MonoBehaviour {
 	
-	Vector3 position;
-	
 	void Start () {
-		position = transform.position;
 	}
 	
 	void Update () {
--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class WorldGenerator : MonoBehaviour {
 	
+	public GameObject[] buildings;
+	
+	public int width, height;
+	public int building_width, building_height;
+	
 	void Awake() {
-		Generate();
+		if (buildings.Length > 0)
+			Generate();
 	}
 	
 	void Generate() {
-		Debug.Log("Generating");
+		for (int i = 0; i < width; ++i)
+			for (int j = 0; j < height; ++j) {
+				Vector3 pos = new Vector3(i * building_width, 0, j * building_height);
+				Instantiate(buildings[0], pos, Quaternion.identity);
+			}
 	}
-	
 }
