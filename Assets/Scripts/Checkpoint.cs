c91ec253c76523380d3b1a79153ad458cb386be1
Donatas <donce.lt@gmail.com>
2013-11-09 17:58:29 +0400

    Checkpoint looking at player, pickable.

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+	
+	GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(player.transform.position);
+	}
+	void OnTriggerEnter(Collider other) {
+		Debug.Log(other);
+		Destroy(gameObject);
+	}
+}
