49a6fd5e86f795c3f63a00afae657b0074b972ca
Donatas <donce.lt@gmail.com>
2013-11-09 23:32:55 +0400

    basic route generation

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -7,27 +7,63 @@ public class WorldGenerator : MonoBehaviour {
 	public int tilesX = 20;
 	public int tilesY = 20;
 	
-	public const int tileWidth = 50;
-	public const int tileHeight = 50;
+	public const int tileWidth = 100;
+	public const int tileHeight = 100;
 	
 	private Checkpoint lastCheckpoint = null;
 	
 	void Awake() {
-		for (int i = 0; i < tilesX; i += 2) {
-			for (int j = 0; j < tilesY; j += 2) {
-				BuildBasic(i, j);
+		bool[,] a = new bool[tilesX, tilesY];
+		for (int i = 0; i < tilesX; ++i)
+			for (int j = 0; j < tilesY; ++j)
+				a[i, j] = true;
+		
+		int cx = tilesX / 2;
+		int cy = tilesY / 2;
+		for (int i = cx-1; i <= cx+1; ++i)
+			for (int j = cy-1; j < cy+1; ++j)
+				a[i, j] = false;
+		GameObject player = GameObject.Find("Player");
+		player.transform.position = getPosition(cx, cy, 50, true);
+		
+		int nx, ny;
+		int px = cx, py = cy;
+		for (int i = 0; i < 10; ++i) {
+			nx = Random.Range(1, tilesX-1);
+			ny = Random.Range(1, tilesY-1);
+			while (nx != px) {
+				px += nx > px ? 1 : -1;
+				a[px, py] = false;
+				Debug.Log(px.ToString() + " " + py.ToString());
 			}
+			while (ny != py) {
+				py += ny > py ? 1 : -1;
+				a[px, py] = false;
+				Debug.Log(px.ToString() + " " + py.ToString());
+			}
+			px = nx;
+			py = ny;
+			AddCheckpoint(nx, ny);
 		}
+		
+		for (int i = 0; i < tilesX; ++i)
+			for (int j = 0; j < tilesY; ++j)
+				if (a[i, j])
+					BuildBasic(i, j);
+		
 		AddCheckpoint(-1, 2);
 		AddCheckpoint(-1, 5);
 		AddCheckpoint(-1, 8);
-		
+	}
+	
+	Vector3 getPosition(int x, int y, int height = 0, bool center = false) {
+		float nx = x + (center ? 0.5f : 0f);
+		float ny = y + (center ? 0.5f : 0f);
+		return new Vector3(nx * tileWidth, height, ny * tileHeight);
 	}
 	
 	Object BuildObject(Object gameObject, int x, int y, int height = 0, bool center = false) {
-		float nx = x + (center ? 0.5f : 0f);
-		float ny = y + (center ? 0.5f : 0f);
-		Vector3 pos = new Vector3(nx * tileWidth, height, ny * tileHeight);
+		Vector3 pos = getPosition(x, y, height, center);
 		return Instantiate(gameObject, pos, Quaternion.identity);
 	}
 	
