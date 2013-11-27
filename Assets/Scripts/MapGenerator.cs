5bc4731e58b27d5da97ead23759540f23cfeb845
KD <karolis.deveikis@mif.stud.vu.lt>
2013-11-27 21:39:53 +0200

    Spice up map generation.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,19 +4,20 @@ using System.Collections.Generic;
 public class MapGenerator : MonoBehaviour {
 
 	/* Prefabs. */
-	public GameObject basicBuilding;
+	public List<GameObject> buildings;
 	public GameObject groundTile;
 	/* Generation parameters. */
-	public int tilesX = 20;
-	public int tilesY = 20;
+	public int tilesX = 40;
+	public int tilesY = 40;
 	public int tileScale = 100;
-	public float rejectRate = 0.5f;
+	public float rejectRate = 0.4f;
+	public float colorVariation = 0.6f;
 	
-	/* List of available spawnpoints. It gets filled in Awake(). */
+	/* List of available spawnpoints. It gets filled in GenerateMap(). */
 	public readonly List<Tile> spawnpoints = new List<Tile>();
 	
 	private System.Random ranGen = new System.Random();
-	
+
 	public class Tile {
 		public readonly int x;
 		public readonly int y;
@@ -33,12 +34,12 @@ public class MapGenerator : MonoBehaviour {
 		for(int x = 0; x < tilesX; x++) {
 			for(int y = 0; y < tilesY; y++) {
 				var tile = new Tile(x, y);
+				PlaceTile(groundTile, tile);
 				if(tileOpen[x, y]) {
-					PlaceTile(groundTile, tile);
 					spawnpoints.Add(tile);
 				}
 				else {
-					PlaceTile(basicBuilding, tile);
+					PlaceBuilding(tile);
 				}
 			}
 		}
@@ -84,12 +85,31 @@ public class MapGenerator : MonoBehaviour {
 		
 		return open;
 	}
-	
+
+	/* Place a building in the specified tile. */
+	private void PlaceBuilding(Tile tile) {
+		var building = buildings[ranGen.Next(buildings.Count)];
+		var instanceRenderer = PlaceTile(building, tile).GetComponentInChildren<Renderer>();
+		instanceRenderer.material.color = RandomColor();
+	}
+
 	/* Place a tile prefab in the specified tile. */
-	private void PlaceTile(Object gameObject, Tile tile) {
-		Vector3 pos = GetUnityPos(tile);
+	private GameObject PlaceTile(Object gameObject, Tile tile) {
+		var pos = GetUnityPos(tile);
 		var instance = Instantiate(gameObject, pos, Quaternion.identity) as GameObject;
 		instance.transform.localScale = new Vector3(tileScale, tileScale, tileScale);
+		return instance;
+	}
+
+	private Color RandomColor() {
+		var r = (float) ranGen.NextDouble() * colorVariation + (1 - colorVariation);
+		var g = (float) ranGen.NextDouble() * colorVariation + (1 - colorVariation);
+		var b = (float) ranGen.NextDouble() * colorVariation + (1 - colorVariation);
+		var mean = (r + g + b) / 3;
+		r = Mathf.Lerp(r, mean, colorVariation);
+		g = Mathf.Lerp(r, mean, colorVariation);
+		b = Mathf.Lerp(r, mean, colorVariation);
+		return new Color(r, g, b);
 	}
 
 }
