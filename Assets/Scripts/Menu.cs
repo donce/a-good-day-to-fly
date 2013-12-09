8d19a8d1ca85ef21048188adff577ea08496292e
Donatas <donce.lt@gmail.com>
2013-12-09 16:02:19 +0200

    menu background, buttons

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,16 +6,21 @@ public class Menu : MonoBehaviour {
 	public Texture2D backgruondTexture;
 
 	void OnGUI() {
-		Debug.Log("OnGUI-");
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgruondTexture);
-		GUI.Box(new Rect(10, 10, 100, 90), "Game");
-		//GUI.DrawTexture(GUI.Window.clientRect, backgruondTexture);
-		if (GUI.Button(new Rect(10, 10, 200, 20), "Start")) {
+		int middle = Screen.width / 2;
+		int vertical = Screen.height / 2;
+
+		if (GUI.Button(new Rect(middle-100, vertical, 200, 20), "Start"))
 			StartClicked();
-		}
+		if (GUI.Button(new Rect(middle-100, vertical+30, 200, 20), "Quit"))
+			QuitClicked();
 	}
 
 	void StartClicked() {
 		Debug.Log("game starts");
+	}
+
+	void QuitClicked() {
+		Debug.Log("Quit clicked");
 	}
 }
